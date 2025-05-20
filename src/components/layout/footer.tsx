208ea480035572863347b89cafb1122471bc11b9
Mir Lim <limmir88@gmail.com>
2025-05-20 15:55:29 +0900

    add footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,8 +1,20 @@
 export default function Footer() {
   return (
-    <div className="bg-gray-100 mx-auto px-4 md:px-6 lg:px-8 py-8">
+    <div className="bg-gray-100 mx-auto px-4 md:px-6 lg:px-20 py-20">
       <footer className="flex h-auto w-full shrink-0 items-center justify-between px-4 md:px-6">
-        <p>© 2025 placesfor.ai</p>
+        <div className="grid grid-cols-12 gap-4 items-baseline box-border w-full">
+          <div className="col-span-3 font-bold text-sm">
+            <div className="flex flex-col gap-2">
+              <a href="/about">About & Features</a>
+              <a href="/contribute">Contribute</a>
+              <a href="/api">API</a>
+              <a href="/ai">AI</a>
+              <a href="/privacy">Privacy</a>
+              <a href="/terms">Terms</a>
+              <a href="/contact">Contact</a>
+            </div>
+          </div>
+        </div>
       </footer>
     </div>
   );
